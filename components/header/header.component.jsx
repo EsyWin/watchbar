8b9993c6e0654110c1fe184facbe6917ca247231
EsyWin <dev@krk.finance>
2022-04-03 13:22:24 +0200

    added header

---
+++ components/header/header.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./header.styles.scss";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://raw.githubusercontent.com/EsyWin/watchbar/main/public/img/watchbar/watchbar_white.png"
+        className="App-logo"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 
+import Header from "../components/header/header.component";
+
 function Homepage(props) {
   return (
     <div>
+      <Header />
       <h1>Welcome to {props.blogTitle}!</h1>
     </div>
   );
