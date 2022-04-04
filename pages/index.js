f2fb3d569819ada53b2710d9b2c1ab8656b1cdb8
bafat123 <87091440+bafat123@users.noreply.github.com>
2022-04-04 16:47:33 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,12 @@ import Footer from "../components/footer/footer.component";
 
 function Homepage(props) {
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div>
-      <h1>Welcome to {props.blogTitle}!</h1>
-      Whaddupp bitchessss
-=======
-=======
->>>>>>> 8ea4390285e7b33de38bf3c5c407e0c8708d6452
     <div className="jumbotron">
       <Header />
       <Ticker />
       <h1 className="text-center title">{props.blogTitle}</h1>
       <Table />
       <Footer />
-<<<<<<< HEAD
->>>>>>> e70376faab11c5ee70e2eec1f8e360aa407c0904
-=======
->>>>>>> 8ea4390285e7b33de38bf3c5c407e0c8708d6452
     </div>
   );
 }
