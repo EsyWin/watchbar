8ea4390285e7b33de38bf3c5c407e0c8708d6452
bafat123 <87091440+bafat123@users.noreply.github.com>
2022-04-04 16:37:28 +0200

    Pull

---
+++ components/header/header.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img
+        src="https://raw.githubusercontent.com/EsyWin/watchbar/main/public/img/watchbar_mini.png"
+        className="App-logo"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Head from "next/head";
+
+import Header from "../components/header/header.component";
+import Ticker from "../components/ticker/ticker.component";
+import Table from "../components/table/table.component";
+import Footer from "../components/footer/footer.component";
 
 function Homepage(props) {
   return (
-    <div>
-      <h1>Welcome to {props.blogTitle}!</h1>
-      Whaddupp bitchessss
+    <div className="jumbotron">
+      <Header />
+      <Ticker />
+      <h1 className="text-center title">{props.blogTitle}</h1>
+      <Table />
+      <Footer />
     </div>
   );
 }
